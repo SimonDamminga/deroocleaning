413b239680d8274ba3fe1fedb18943cb8f1bbf4b
Simon Damminga <simon.damminga@gmail.com>
2026-08-22 15:25:28 +0200

    Added terms and conditions and privacy statement

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ export const routes: Routes = [
     { path: 'services', loadComponent: () => import('./components/services/services').then(m => m.Services), title: 'Prijzen' },
     { path: 'about', loadComponent: () => import('./components/about/about').then(m => m.About), title: 'Over ons' },
     { path: 'contact', loadComponent: () => import('./components/contact/contact').then(m => m.Contact), title: 'Contact' },
-    { path: 'terms', loadComponent: () => import('./components/terms/terms').then(m => m.Terms), title: 'Terms'},
+    { path: 'terms', loadComponent: () => import('./components/terms/terms').then(m => m.Terms), title: 'Algemene voorwaarden'},
+    { path: 'privacy', loadComponent: () => import('./components/privacy/privacy').then(m => m.Privacy), title: 'Privacyverklaring'},
     { path: '**', redirectTo: 'home' },
 ];
--- src/app/components/terms/terms.ts
+++ src/app/components/terms/terms.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-terms',
-  imports: [MatTabsModule],
+  imports: [],
   templateUrl: './terms.html',
   styleUrl: './terms.scss',
 })
